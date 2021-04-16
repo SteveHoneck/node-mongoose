cf7604ae54add6e28bc5317baab406c83d8669b5
SteveDev <stevehoneck@gmail.com>
2021-04-15 21:25:21 -0400

    Post 'Exercise: Mongoose ODM Part 3'

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const Campsite = require('./models/campsite'); //Require the Campsite model as "
 const url = 'mongodb://localhost:27017/nucampsite'; //shortcut for URL of the MongoDB server. This URL will automatically connect us to the "nucampsite" database in the MongoDB server
 const connect = mongoose.connect(url, { //1st argument is the URL of the database
     //2nd argument is an object to set options, being set to deal with some depracation warnings
-    useCreateIndex: true, 
+    useCreateIndex: true,
+    useFindAndModify: false, 
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -26,10 +27,26 @@ connect.then(() => { //the "mongoose.connect" method returns a promise, so a ".t
 
     .then(campsite => { //Takes the resolved promise from ".create" and names it "campsite", passes it to the console.log to show what has been made and saved
         console.log(campsite);
-        return Campsite.find(); //use ".find" with capital C (referrs to the Model), to look for all documents that are based on the Campsite model, return the result as a promise which if successful will return the found documents inside an array of objects.
+        //update the campsite itself. 
+        return Campsite.findByIdAndUpdate(campsite._id, { //Give 3 arguments, the document's ID 
+            $set: { description: 'Updated Test Document' } //2nd argument an object with the update operator ($set) and specify what field to change, 
+        }, {
+            new: true //3rd argument set option of "new: true" which will cause the method to return the updated document (the default is to return the original document before it was updated)
+        });
     })
-    .then(campsites => { //take the array of objects from ".find" and console.log it
-        console.log(campsites);
+    .then(campsite => { //Promise from "findByIdAndUpdate" resolves into a campsite document and is assigned to "campsite"
+        console.log(campsite);
+
+        campsite.comments.push({ //Comments subdocuments are stored in the Campsite document as an array, so can use "push" and pass it an object that defines the subdocument.
+            rating: 5,
+            text: 'What a magnificent view!',
+            author: 'Tinus Lorvaldes'
+        });
+
+        return campsite.save(); //Return the "campsite.save()" method in order for the subdocument update to take affect, it will return the campsite that was saved with the new Comments sub document
+    })
+    .then(campsite => { //takes the single campsite document resolved from "campsite.save()" 
+        console.log(campsite);
         return Campsite.deleteMany(); //call the "deleteMany" method on all documents that use the "Campsite" model
     })
     .then(() => { //Close the connection
--- models/campsite.js
+++ models/campsite.js
@@ -3,6 +3,26 @@
 const mongoose = require('mongoose'); //Import 'mongoose'
 const Schema = mongoose.Schema; //THis line makes a shorthand to the "mongoose.Schema" function so it can be referred to as just "Schema"
 
+const commentSchema = new Schema({ //New Schema which will be used for documents storing comments about a campsite.
+    rating: {
+        type: Number,
+        min: 1, //min, max are able to be set if type is "Number"
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+
 //Create the schema
 const campsiteSchema = new Schema({ //This creates a new object named "campsiteSchema", provide the schema with 2 arguments 
     //1st argument is required, it is an object that contains the definition for the schema via the object properties
@@ -14,7 +34,8 @@ const campsiteSchema = new Schema({ //This creates a new object named "campsiteS
     description: { //2nd property 
         type: String, //will be type string
         required: true// will be required
-    }
+    },
+    comments: [commentSchema] //Add "commentSchema" as a sub-document. Putting it in an array will cause every campsite document to be able to contain multiple comment documents stored within an array
 }, { //2nd argument is optional which sets various configuration options
     timestamps: true //cause mongoose automatically add 2 properties to this schema called "createdAt" and "updatedAt", which will be managed by Mongoose. When a document is created by the schema, it will be given the "createdAt" and "updateAt", whenever it's updated, "updatedAt" will be updated by Mongoose.
 });
